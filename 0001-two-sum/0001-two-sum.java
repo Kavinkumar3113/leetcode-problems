25b139e670435b64fa374ae8c543d2297548a37e
Kavinkumar3113 <146343179+Kavinkumar3113@users.noreply.github.com>
2024-08-11 16:34:00 +0530

    Time: 0 ms (100%), Space: 44.9 MB (27.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,19 +3,19 @@ class Solution {
    
         int array[]=new int[2];
         hes:
-        for(int i=0;i<nums.length;i++)
+        for(int i=1;i<nums.length;i++)
             {
-                for(int j=1;j<nums.length;j++)
+                for(int j=i;j<nums.length;j++)
                 {
-                    if(nums[i]+nums[j]==target)
+                    if(nums[j]+nums[j-i]==target)
                     {
 
-                    if(i!=j)
-                    {
-                        array[0]=i;
+                   
+                  
+                        array[0]=j-i;
                         array[1]=j;
                         break hes;
-                    }
+                 
                     }
                 }
             }
