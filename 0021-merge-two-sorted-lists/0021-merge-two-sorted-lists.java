02c7a85005b023c21a2cbdd1f3bbd84392b41762
Kavinkumar <146343179+Kavinkumar3113@users.noreply.github.com>
2025-02-10 10:42:59 +0530

    Time: 0 ms (100%), Space: 42.8 MB (34.11%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,45 +10,43 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1==null&&list2==null)
+         if(list1==null&& list2==null)
            return null;
-        if(list1==null)
-          return list2;
-        if(list2==null)
-          return list1;
-        ListNode temp,head;
-        if(list1.val<list2.val)
-        {
-            temp=list1;
-            head=temp;
-            list1=list1.next;
-        }
-        else
-        {
-            temp=list2;
-            head=temp;
-            list2=list2.next;
-        }
+        if(list1==null&& list2!=null)
+           return list2;
+        if(list1!=null&& list2==null)
+           return list1;
+        ListNode ans=new ListNode();
+        ListNode temp=ans;
+
         while(list1!=null&&list2!=null)
         {
-            if(list1.val<list2.val)
+            while(list1.val<=list2.val)
             {
-                  temp.next=list1;
-                  list1=list1.next;
-                  temp=temp.next;
-            }
-            else
-            {   temp.next=list2;
-                list2=list2.next;
+                temp.next=new ListNode(list1.val);
                 temp=temp.next;
+                if(list1.next==null)
+                    {
+                        temp.next=list2;
+                          return ans.next;
+                    }
+                list1=list1.next;
+               
+            }
+             while(list1.val>list2.val)
+            {
+                temp.next=new ListNode(list2.val);
+                temp=temp.next;
+                 if(list2.next==null)
+                    {
+                        temp.next=list1;
+                          return ans.next;
+                    }
+                list2=list2.next;
+                
             }
         }
-        if(list1!=null)
-            temp.next=list1;
-        if(list2!=null)
-        {
-            temp.next=list2;
-        }
-        return head;
+        
+        return ans.next;
     }
 }
