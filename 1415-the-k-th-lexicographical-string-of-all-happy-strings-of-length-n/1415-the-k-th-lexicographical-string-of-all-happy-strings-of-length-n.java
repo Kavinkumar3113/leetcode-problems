14e33ca6171126db0391aa66f1610ae057f2e9d6
Kavinkumar <146343179+Kavinkumar3113@users.noreply.github.com>
2025-02-19 15:15:07 +0530

    Time: 16 ms (44.42%), Space: 45.6 MB (9.11%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,24 +1,26 @@
 class Solution {
     List<String> list=new ArrayList<>();
     
-    public void solve(int n,String s)
+    public void solve(int n,String s,int k)
     {
        if(s.length()==n)
           {
             list.add(s);
             return;
           }
+        if(list.size()==k)
+        return;
         for(char c='a';c<='c';c++)
         {
             int v=s.length();
             if(v>0 &&
                s.charAt(v-1)==c)
                continue;
-               solve(n,s+c);
+               solve(n,s+c,k);
         }
     }
     public String getHappyString(int n, int k) {
-        solve(n,"");
+        solve(n,"",k);
         if(list.size()<k)
         return "";
         return list.get(k-1);
